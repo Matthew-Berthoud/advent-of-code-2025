32dbfd8dfc664cfd5c8274509fe7e2fb59fd2cef
Matthew Berthoud <matthewberthoud@gmail.com>
2025-12-11 17:33:01 -0500

    part 1 done

---
+++ 11/reactor.py
@@ -0,0 +1,33 @@
+START = "you"
+END = "out"
+
+with open("input.txt") as f:
+    lines = [line.strip().split() for line in f.readlines()]
+
+
+devices = {}
+for line in lines:
+    device = line.pop(0)[:-1]
+    outputs = line
+    devices[device] = outputs
+
+count = [0]
+
+
+def get_outputs(start):
+    outputs = devices[start]
+    for out in outputs:
+        print(f"From {start} to {out}")
+        if out != END:
+            outputs = get_outputs(out)
+        else:
+            print()
+            count[0] += 1
+    return outputs
+
+
+# outputs = devices[START]
+
+get_outputs(START)
+
+print(count[0])
